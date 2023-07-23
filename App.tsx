5c03b8444aba2fb549db1df6022536100589917e
Gleison Souza <101277114+gleisoncoruja@users.noreply.github.com>
2023-07-23 20:46:13 -0300

    feat: navigation bar created

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "react-native";
+
+import { NavigationContainer, TabRouter } from "@react-navigation/native";
+import TabsNavigator from "./src/routes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar
+        backgroundColor={"#000"}
+        barStyle="light-content"
+        translucent={false}
+      />
+      <TabsNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Expense/index.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function ExpenseScreen() {
+  return (
+    <View>
+      <Text>Página de despesas</Text>
+    </View>
+  );
+}
